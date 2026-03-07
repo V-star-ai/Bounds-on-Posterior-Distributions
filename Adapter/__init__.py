6f41d0ac72e77fada685568778d3a7e470c52309
RhineGST <GST49151709884@outlook.com>
2026-03-07 15:20:28 +0800

    connect ipopt

--- Adapter/__init__.py
+++ Adapter/__init__.py
@@ -1,4 +1,5 @@
 from Adapter.adapter import Adapter
 from Adapter.z3_adapter import Z3Adapter
+from Adapter.ipopt_adapter import IpoptAdapter
 
-__all__ = ["Adapter", "Z3Adapter"]
+__all__ = ["Adapter", "Z3Adapter", "IpoptAdapter"]
