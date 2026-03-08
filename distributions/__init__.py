f757ee3ffb1ab37eda0ad3b878137f1a13275a3e
V-star-ai <mingqiyang@zju.edu.cn>
2026-03-08 14:11:53 +0800

    Update __init__.py

--- distributions/__init__.py
+++ distributions/__init__.py
@@ -1,4 +1,6 @@
-from distributions.normal import Normal
 from distributions.EED import EED
+from distributions.normal import Normal
+from distributions.uniform import Uniform
+from distributions.exponential import Exponential
 
-__all__ = ["Normal", "EED"]
+__all__ = ["EED", "Normal", "Uniform", "Exponential"]
